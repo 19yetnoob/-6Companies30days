2e037933d08b9d38a6b5aa3e7bdaafb18c67add4
TUSHAR BAIRI <72160708+tushar-coin@users.noreply.github.com>
2022-02-01 01:09:21 +0530

    Update 9.cpp

--- adobe/9.cpp
+++ adobe/9.cpp
@@ -1,56 +1,18 @@
-class Solution {
-public:
-
-	bool isValid(int x, int y, int n, int m){
-        return !(x < 0 || y < 0 || x >= n || y >= m);
-    }
-	
-    void bfs(int n, int m, queue<pair<int,int>>& q, vector<vector<bool>> &visited, vector<vector<int>> heights){
-        int dx[] = {-1,0,0,1};
-        int dy[] = {0,-1,1,0};
-        while(!q.empty()){
-            auto ele = q.front();
-            q.pop();
-            for(int iter = 0 ; iter < 4 ; iter++){
-                int nx = ele.first+dx[iter];
-                int ny = ele.second+dy[iter];
-                if(isValid(nx,ny,n,m) && heights[ele.first][ele.second] <= heights[nx][ny] && !visited[nx][ny]){
-                    q.push({nx,ny});
-                    visited[nx][ny] = true;
-                }
+    string nextPalin(string s) { 
+        //complete the function here
+       int n=s.size();
+        if(n<=3) return "-1";
+        string half = s.substr(0,n/2);
+        if( next_permutation( half.begin() , half.end() ) )
+        {
+            string ans=half;
+            if(n&1)
+            {
+                ans+=s[n/2];
             }
+            reverse(half.begin() , half.end());
+            ans+=half;
+            return ans;
         }
+        return "-1";
     }
-	
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int m = heights[0].size();
-		queue<pair<int,int>> atlantic_queue;
-        queue<pair<int,int>> pacific_queue;
-        vector<vector<bool>> atlantic(n,vector<bool>(m,false));
-        vector<vector<bool>> pacific(n,vector<bool>(m,false));
-        vector<vector<int>> res;
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < m; j++){
-                if(i == 0 || j == 0){
-                    pacific_queue.push({i,j});
-                    pacific[i][j] = true;
-                }
-                if(i == n-1 || j == m-1){
-                    atlantic_queue.push({i,j});
-                    atlantic[i][j] = true;
-                }
-            }
-        }
-        bfs(n, m, atlantic_queue, atlantic, heights);
-        bfs(n, m, pacific_queue, pacific, heights);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(atlantic[i][j] && pacific[i][j]){
-                    res.push_back({i,j});
-                }
-            }
-        }
-        return res;
-    }
-};
